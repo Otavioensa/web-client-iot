8655cc494dbaae3d0d93aabd417f4878e5c35b45
Otavio <otavioensa@gmail.com>
2016-05-22 22:42:43 -0300

    Inclusão de client e server com socket, capturando e dando display nas mensagens que chegam do dweetio

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 
   var express = require('express');
   var app = express();
+  var http = require('http').Server(app);
   var bodyParser = require('body-parser');
+  var io = require('socket.io')(http);
+  var dweetClient = require('node-dweetio');
+  var dweetio = new dweetClient();
 
   app.use(bodyParser.urlencoded({ extended: true }));
   app.use(bodyParser.json());
@@ -17,7 +21,22 @@
     });
   });
 
-  app.listen(process.env.PORT || 3000, function () {
+  io.on('connection', function(socket){
+    
+    console.log('A connection has benn established');
+    socket.on('disconnect', function(){
+      console.log('user disconnected');
+    });
+
+  });
+
+  dweetio.listen_for('web-client-iot-cs', function (dweet) {
+    console.log(dweet.content);
+    io.emit('sensor-data', dweet.content);
+  });
+
+
+  http.listen(process.env.PORT || 3000, function () {
   	console.log('Servidor no ar');
   });
 
