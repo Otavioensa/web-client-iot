96b800dfc78dada8bb464fdf03fd399f61140c77
Otavio <otavioensa@gmail.com>
2016-05-30 22:47:38 -0300

    Final version

--- app.js
+++ app.js
@@ -13,37 +13,33 @@
   app.use(express.static('public'));
 
   app.get('/', function (req, res) {
-  	res.sendFile(__dirname + 'index.html');
+    res.sendFile(__dirname + 'index.html');
   });
 
   app.use(function (req, res) {
-  	 res.status(404).json({
-      mensagem: 'Recurso não existente'
+    res.status(404).json({
+      mensagem: 'Resource not found'
     });
   });
 
-  io.on('connection', function(socket){
-    
+  io.on('connection', function(socket){ 
     console.log('A connection has benn established');
     socket.on('disconnect', function(){
-      console.log('user disconnected');
+      console.log('User disconnected');
     });
-
   });
 
-  dweetio.listen_for('web-client-iot-cs', function (dweet) {
-    
+  dweetio.listen_for('web-client-iot-cs', function (dweet) {  
     var data = {
       sensorData: dweet.content,
       time: moment().format('HH:mm:ss')
     };
-
     io.emit('sensor-data', data);
   });
 
 
   http.listen(process.env.PORT || 3000, function () {
-  	console.log('Servidor no ar');
+    console.log('Server on');
   });
 
 
